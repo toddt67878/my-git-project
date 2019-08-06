827225656ce3ef7241dbabd0aafbb9fa79171381
Todd Traylor <toddt67878@yahoo.com>
2019-08-06 14:16:55 -0500

    git cache cleared

--- team-three-donut-shop.git/src/components/app.js
+++ team-three-donut-shop.git/src/components/app.js
@@ -1,9 +1,5 @@
 import React, { Component } from "react";
 
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
-import Auth from "./pages/auth";
-
 export default class App extends Component {
   render() {
     return (
